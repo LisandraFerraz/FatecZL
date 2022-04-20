e4250b7f9ff37872c38e0e2ff13e0a2a8ac026f3
Lisandra Ferraz <61096682+LisandraFerraz@users.noreply.github.com>
2022-04-20 19:41:41 -0300

    Update Form1.vb

--- Trabalhos com Visual Basic/projeto_04/Form1.vb
+++ Trabalhos com Visual Basic/projeto_04/Form1.vb
@@ -8,7 +8,7 @@
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("Você clicou no boitão!")
+        MsgBox("Você clicou no botão!")
 
     End Sub
 
