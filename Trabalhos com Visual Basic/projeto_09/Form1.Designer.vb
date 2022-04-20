b18df91080b3a019cb28d756a3f59a58f101b635
Lisandra Ferraz <61096682+LisandraFerraz@users.noreply.github.com>
2022-04-20 19:40:24 -0300

    Add files via upload

---
+++ Trabalhos com Visual Basic/projeto_09/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnLimpar = New System.Windows.Forms.Button()
+        Me.btnSair = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtBPeso = New System.Windows.Forms.TextBox()
+        Me.TxtBAltura = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnLimpar
+        '
+        Me.btnLimpar.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnLimpar.Location = New System.Drawing.Point(255, 263)
+        Me.btnLimpar.Name = "btnLimpar"
+        Me.btnLimpar.Size = New System.Drawing.Size(113, 34)
+        Me.btnLimpar.TabIndex = 5
+        Me.btnLimpar.Text = "Limpar"
+        Me.btnLimpar.UseVisualStyleBackColor = True
+        '
+        'btnSair
+        '
+        Me.btnSair.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnSair.Location = New System.Drawing.Point(406, 263)
+        Me.btnSair.Name = "btnSair"
+        Me.btnSair.Size = New System.Drawing.Size(113, 34)
+        Me.btnSair.TabIndex = 6
+        Me.btnSair.Text = "Sair"
+        Me.btnSair.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.btnCalcular.Location = New System.Drawing.Point(104, 263)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(113, 34)
+        Me.btnCalcular.TabIndex = 7
+        Me.btnCalcular.Text = "Calcular IMC"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(104, 151)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 21)
+        Me.Label1.TabIndex = 8
+        Me.Label1.Text = "Digite seu peso"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label2.Location = New System.Drawing.Point(104, 200)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(127, 21)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Digite sua altura"
+        '
+        'txtBPeso
+        '
+        Me.txtBPeso.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtBPeso.Location = New System.Drawing.Point(272, 151)
+        Me.txtBPeso.Name = "txtBPeso"
+        Me.txtBPeso.Size = New System.Drawing.Size(247, 29)
+        Me.txtBPeso.TabIndex = 10
+        '
+        'TxtBAltura
+        '
+        Me.TxtBAltura.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.TxtBAltura.Location = New System.Drawing.Point(272, 200)
+        Me.TxtBAltura.Name = "TxtBAltura"
+        Me.TxtBAltura.Size = New System.Drawing.Size(247, 29)
+        Me.TxtBAltura.TabIndex = 11
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label3.Location = New System.Drawing.Point(212, 68)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(204, 30)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Calculadora de IMC"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(638, 403)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TxtBAltura)
+        Me.Controls.Add(Me.txtBPeso)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.btnSair)
+        Me.Controls.Add(Me.btnLimpar)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnLimpar As Button
+    Friend WithEvents btnSair As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtBPeso As TextBox
+    Friend WithEvents TxtBAltura As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Trabalhos com Visual Basic/projeto_09/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+    Private Sub btnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Dim peso As Decimal = CDec(txtBPeso.Text)
+        Dim altura As Decimal = CDec(TxtBAltura.Text)
+    End Sub
+End Class
