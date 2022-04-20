75117989722e4c430c1414c568ef45b8dfe26d10
Lisandra Ferraz <61096682+LisandraFerraz@users.noreply.github.com>
2022-04-20 19:39:27 -0300

    Add files via upload

---
+++ Trabalhos com Visual Basic/projeto_04/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Você clicou no boitão!")
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MsgBox(TextBox1.Text)
+        Label4.Text = (TextBox1.Text)
+    End Sub
+
+    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
+
+    End Sub
+End Class
